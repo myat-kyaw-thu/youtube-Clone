8b417b75d42222a67c17a0a6b878a3420143d8e7
myat kyaw thu <149857208+myat-kyaw-thu@users.noreply.github.com>
2026-03-31 19:22:17 +0630

    chore: add missing newline in Form1

--- Form1.cs
+++ Form1.cs
@@ -156,3 +156,5 @@ namespace GreenLifeOrganicStore
 // micro-commit 294: chore: add missing newline in Form1
 
 // micro-commit 298: refactor: clean up Form1
+
+// micro-commit 302: chore: add missing newline in Form1
