82d955a025b7c1e8f619c591e5a57a6b8f87ea9b
myat kyaw thu <149857208+myat-kyaw-thu@users.noreply.github.com>
2026-04-07 20:17:02 +0630

    test: cover edge cases in Form1.Designer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -140,3 +140,5 @@
 // micro-commit 221: chore: sync Form1.Designer with latest template
 
 // micro-commit 225: chore: sync Form1.Designer with latest template
+
+// micro-commit 229: test: cover edge cases in Form1.Designer
