f9088761b1225ffac7e091692ce17ac21cd888c3
myat kyaw thu <149857208+myat-kyaw-thu@users.noreply.github.com>
2026-04-09 20:30:32 +0630

    test: cover edge cases in Program

--- Program.cs
+++ Program.cs
@@ -110,3 +110,5 @@ namespace GreenLifeOrganicStore
 // micro-commit 203: fix: handle edge case in Program
 
 // micro-commit 207: style: apply consistent indentation in Program
+
+// micro-commit 211: test: cover edge cases in Program
