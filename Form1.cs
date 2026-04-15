93db5ec1df63e09cac177fc4699eab1977dd8261
myat kyaw thu <149857208+myat-kyaw-thu@users.noreply.github.com>
2026-04-15 21:16:17 +0630

    docs: update README for Form1

--- Form1.cs
+++ Form1.cs
@@ -80,3 +80,5 @@ namespace GreenLifeOrganicStore
 // micro-commit 142: fix: patch off-by-one error in Form1
 
 // micro-commit 146: chore: sync Form1 with latest template
+
+// micro-commit 150: docs: update README for Form1
