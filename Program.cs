174ab92e279d76948aa11e8f86b3ef82bd16cfef
myat kyaw thu <149857208+myat-kyaw-thu@users.noreply.github.com>
2026-04-23 22:15:32 +0630

    chore: add missing newline in Program

--- Program.cs
+++ Program.cs
@@ -40,3 +40,5 @@ namespace GreenLifeOrganicStore
 // micro-commit 63: chore: update Program dependencies
 
 // micro-commit 67: chore: add missing newline in Program
+
+// micro-commit 71: chore: add missing newline in Program
