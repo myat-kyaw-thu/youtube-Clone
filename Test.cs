802e84330f82089a271da0e799328f8c5f7dbd2b
myat kyaw thu <149857208+myat-kyaw-thu@users.noreply.github.com>
2026-04-28 22:33:02 +0630

    feat: wire up Test to data layer

---
+++ Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Test {
+    static void Main() {
+        string password = "password";
+        byte[] saltBytes = new byte[32];
+        using (var rng = new RNGCryptoServiceProvider()) {
+            rng.GetBytes(saltBytes);
+        }
+        string salt = Convert.ToBase64String(saltBytes);
+        string combined = password + salt;
+        using (var sha256 = SHA256.Create()) {
+            byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(combined));
+            string hash = Convert.ToBase64String(hashBytes);
+            Console.WriteLine(salt + ":" + hash);
+        }
+    }
+}
